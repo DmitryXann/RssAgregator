ad0ddc2cfeb4952a788a1ca2a12fccf4db9fafd1
Dmitry <mailrules@mail.ru>
2016-04-20 02:12:06 +0300

    settings deploy scripts

--- RssAgregator.DAL/RssAggregatorDeployScripts.sql
+++ RssAgregator.DAL/RssAggregatorDeployScripts.sql
@@ -1054,3 +1054,65 @@ BEGIN
 END
 GO
 --========================================================
+--========================================================
+DECLARE @Key nvarchar(255)
+	   ,@Value nvarchar(max)
+	   ,@ForUI bit
+
+SELECT @Key		= N'VideoJSCantPlay'
+	   ,@Value	= N'To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>'
+	   ,@ForUI  = 1
+
+IF EXISTS(SELECT * FROM [dbo].[SettingsSet] WHERE [Key] like @Key)
+BEGIN
+	UPDATE [dbo].[SettingsSet]
+	SET
+		 [Key]		= @Key
+		,[Value]	= @Value
+		,[ForUI]    = @ForUI
+	WHERE [Key]	= @Key
+END
+ELSE
+BEGIN
+	INSERT INTO [dbo].[SettingsSet]
+		([Key]
+		,[Value]
+		,[ForUI])
+	VALUES
+		(@Key
+		,@Value
+		,@ForUI)
+END
+GO
+--========================================================
+--========================================================
+DECLARE @Key nvarchar(255)
+	   ,@Value nvarchar(max)
+	   ,@ForUI bit
+
+SELECT @Key		= N'VideoJSNotSupportedFormat'
+	   ,@Value	= N'This video fromat is not supported, please check http://videojs.com/html5-video-support/ for supported HTML5 video formats</a>'
+	   ,@ForUI  = 1
+
+IF EXISTS(SELECT * FROM [dbo].[SettingsSet] WHERE [Key] like @Key)
+BEGIN
+	UPDATE [dbo].[SettingsSet]
+	SET
+		 [Key]		= @Key
+		,[Value]	= @Value
+		,[ForUI]    = @ForUI
+	WHERE [Key]	= @Key
+END
+ELSE
+BEGIN
+	INSERT INTO [dbo].[SettingsSet]
+		([Key]
+		,[Value]
+		,[ForUI])
+	VALUES
+		(@Key
+		,@Value
+		,@ForUI)
+END
+GO
+--========================================================
