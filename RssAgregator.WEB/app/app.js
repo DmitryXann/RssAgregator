912e8ae5725cf0dddd7dcf6734bbb54e979dcc5c
Dmitry <mailrules@mail.ru>
2016-04-20 02:13:09 +0300

    add init to an app.js to have an one global init location

--- RssAgregator.WEB/app/app.js
+++ RssAgregator.WEB/app/app.js
@@ -8,12 +8,9 @@
         'ngSanitize',
         'ngTouch',
         'ui.bootstrap'
-    ]).run(['$rootScope', function ($rootScope) {
-        // Global error handler
-        //window.onerror = function (msg, url, line) {
-        //    //toastr.error("Error: " + msg + "\nurl: " + url + "\nline #: " + line);
-        //    return true;
-        //};
+    ]).run(['$rootScope', '$log',
+        function ($rootScope, $log) {
+
         function decimalAdjust(type, value, exp) {
             if (angular.isUndefined(exp) || +exp === 0) {
                 return Math[type](value);
@@ -34,10 +31,42 @@
             return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
         }
 
-        if (!Math.round10) {
-            Math.round10 = function (value, exp) {
-                return decimalAdjust('round', value, exp);
+        function init() {
+            //global round10 Math prototype
+            if (!Math.round10) {
+                Math.round10 = function (value, exp) {
+                    return decimalAdjust('round', value, exp);
+                };
+            }
+
+            //global format String prototype
+            if (!String.prototype.format) {
+                String.prototype.format = function () {
+                    var args = arguments;
+
+                    return this.replace(/{(\d+)}/g, function (match, number) {
+                        return !angular.isUndefined(args[number]) ? args[number] : match;
+                    });
+                };
+            }
+
+            //global error handling
+            window.onerror = function (errorMsg, url, lineNumber) {
+                $log.error('An error ha occurred: {0}, url: {1}, line: {3}'.format(errorMsg, url, lineNumber));
+                return false; //TODO: change to true
             };
+
+            //videojs init
+            videojs.options.flash.swf = '/Scripts/video-js-5.9.2/video-js.swf';
+
+            //soundmanagerv2 init
+            soundManager.setup({
+                url: '/Scripts/soundmanagerv2/',
+                flashVersion: 9,
+                preferFlash: false
+            });
         }
+
+        init();
     }]);
 })(angular);
