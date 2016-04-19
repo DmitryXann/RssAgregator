9a48d409a4c7cc105d76f0c2c5c5c5728ebbf2ec
Dmitry <mailrules@mail.ru>
2016-04-20 00:46:36 +0300

    video gallery additional file

---
+++ RssAgregator.WEB/app/directives/containers/videoGalleryContainer/videoGalleryContainer.js
@@ -0,0 +1,18 @@
+﻿; (function (angular) {
+    'use strict';
+
+    angular.module('app')
+		.directive('videoGalleryContainer', ['$filter',
+		function ($filter) {
+		    return {
+		        restrict: 'E',
+		        templateUrl: 'app/directives/containers/videoGalleryContainer/videoGalleryContainer.html',
+		        transclude: true,
+		        scope: {
+		        },
+		        link: function (scope, element, attrs, ctrl, transclude) {
+		           
+		        }
+		    };
+		}]);
+})(angular);
