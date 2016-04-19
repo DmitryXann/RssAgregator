b39fc7ed2004e7dbc2e40c52a0b184a36038de34
hereandout <hereandout@gmail.com>
2016-04-19 16:54:20 +0300

    Template for TemplateSet, Deploye scripts correction + new local connection string

--- RssAgregator.DAL/RssAggregatorDeployScripts.sql
+++ RssAgregator.DAL/RssAggregatorDeployScripts.sql
@@ -754,6 +754,52 @@ END
 GO
 --========================================================
 --========================================================
+DECLARE @Name nvarchar(max)
+	   ,@Description nvarchar(max)
+	   ,@View nvarchar(max)
+	   ,@Version int
+	   ,@Type tinyint
+	   ,@User_Id int
+
+SELECT @Name			= N'TempHeader'
+	   ,@Description	= N'Temp header'
+	   ,@View			= N'<post-container ng-repeat="post in postList" post-id="{{post.Id}}" post-object="post"></post-container>'
+	   ,@Version		= NULL
+	   ,@Type			= 2
+	   ,@User_Id		= 1
+
+IF EXISTS(SELECT * FROM [dbo].[TemplateSet] WHERE [Name] like @Name)
+BEGIN
+	UPDATE [dbo].[TemplateSet]
+	SET
+		 [Name]			= @Name
+		,[Description]	= @Description
+		,[View]			= @View
+		,[Version]		= @Version
+		,[Type]			= @Type
+		,[User_Id]		= @User_Id
+	WHERE [Name]		= @Name
+END
+ELSE
+BEGIN
+	INSERT INTO [dbo].[TemplateSet]
+		([Name]
+		,[Description]
+		,[View]
+		,[Version]
+		,[Type]
+		,[User_Id])
+	VALUES
+		(@Name
+		,@Description
+		,@View
+		,@Version
+		,@Type
+		,@User_Id)
+END
+GO
+--========================================================
+--========================================================
 DECLARE @Key nvarchar(255)
 	   ,@Value nvarchar(max)
 	   ,@ForUI bit
