fbc5f3a7d2a484b69fcdf870c950c7da03a5d23a
Dmitry <mailrules@mail.ru>
2016-06-07 03:02:50 +0300

    update angular and uiBootsrap to a new version

--- RssAgregator.WEB/App_Start/BundleConfig.cs
+++ RssAgregator.WEB/App_Start/BundleConfig.cs
@@ -12,24 +12,24 @@ namespace RssAgregator.WEB
 
             bundles.Add(new ScriptBundle("~/bundles/jquerry").Include("~/Scripts/jquerry-2.2.2/jquery-2.2.2.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquerry-plugins").Include("~/Scripts/jquerry-plugins/md5/jquery.md5.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquerry-plugins").Include("~/Scripts/jquerry-plugins/md5/jquery.md5.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/toastr").Include("~/Scripts/toastr/toastr.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                        "~/Scripts/angular-1.4.8/angular-loader.js",
-                        "~/Scripts/angular-1.4.8/angular.js",
-                        "~/Scripts/angular-1.4.8/angular-animate.js",
-                        "~/Scripts/angular-1.4.8/angular-resource.js",
-                        "~/Scripts/angular-1.4.8/angular-route.js",
-                        "~/Scripts/angular-1.4.8/angular-sanitize.js",
-                        "~/Scripts/angular-1.4.8/angular-touch.js"));
+                        "~/Scripts/angular-1.5.6/angular-loader.js",
+                        "~/Scripts/angular-1.5.6/angular.js",
+                        "~/Scripts/angular-1.5.6/angular-animate.js",
+                        "~/Scripts/angular-1.5.6/angular-resource.js",
+                        "~/Scripts/angular-1.5.6/angular-route.js",
+                        "~/Scripts/angular-1.5.6/angular-sanitize.js",
+                        "~/Scripts/angular-1.5.6/angular-touch.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/ui-bootstrap").Include("~/Scripts/ui-bootstrap-1.2.5/ui-bootstrap-tpls-1.2.5.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/ui-bootstrap").Include("~/Scripts/ui-bootstrap-1.3.3/ui-bootstrap-tpls-1.3.3.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/soundmanagerv2").Include("~/Scripts/soundmanagerv2/soundmanager2-nodebug-jsmin.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/videojs").Include("~/Scripts/video-js-5.9.2/video.js").IncludeDirectory("~/Scripts/video-js-5.9.2/lang/", "*.js", true));
+            bundles.Add(new ScriptBundle("~/bundles/videojs").Include("~/Scripts/video-js-5.9.2/video.min.js").IncludeDirectory("~/Scripts/video-js-5.9.2/lang/", "*.js", true));
 
             bundles.Add(new ScriptBundle("~/bundles/content-tools").Include("~/Scripts/content-tools/content-tools.js"));
 
@@ -39,7 +39,7 @@ namespace RssAgregator.WEB
                         "~/Content/libs/bootstrap-3.3.6/bootstrap-theme.css",
                         "~/Content/libs/bootstrap-3.3.6/bootstrap.css",
                         "~/Content/libs/toastr/toastr.css",
-                        "~/Content/libs/angular-1.4.8/angular-csp.css",
+                        "~/Content/libs/angular-1.5.6/angular-csp.css",
                         "~/Content/libs/video-js-5.9.2/video-js.css",
                         "~/Content/libs/content-tools/content-tools.min.css"));
 
